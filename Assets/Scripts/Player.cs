90e14a7f76cbc842fdc5cecbc02f489a80993ac0
Ksoucha <144827110+Ksoucha@users.noreply.github.com>
2024-10-25 16:07:54 -0400

    PV system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,9 +8,13 @@ public class Player : MonoBehaviour
     public float speed = 5f;
     public Rigidbody2D rb;
 
+    public TMP_Text pvText;
+    public float pv;
+
     void Start()
     {
-   
+        pvText.text = "PV : 100";
+        pv = 100;
     }
 
     void Update()
@@ -24,5 +29,8 @@ public class Player : MonoBehaviour
         {
             rb.velocity = Vector2.up * 5;
         }
+
+        pv -= 1 * Time.deltaTime;
+        pvText.text = "PV : " + Mathf.Round(pv);
     }
 }
